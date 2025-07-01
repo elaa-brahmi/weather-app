9760ac724829ccf4f696117f1b8e6fb02a9fe3be
elaa <elaa.brahmii@gmail.com>
2025-07-01 18:47:41 +0100

    fetch weather infos

--- components/home/InputField.tsx
+++ components/home/InputField.tsx
@@ -3,39 +3,66 @@ import {MapPinIcon, Search} from 'lucide-react';
 import { Input } from '../ui/input';
 import { useState } from 'react';
 import { Button } from '../ui/button';
+import WeatherCard from './WeatherCard';
 export default function InputField() {
-    const [city, setCity] = useState('');
+    const [Thecity, setCity] = useState('');
+     const initialWeather = {
+        city: '',
+        temperature: '',
+        description: '',
+        condition: '',
+        icon: '',
+        humidity: '',
+        windSpeed: '',
+    };
+    const [Theweather, setWeather] = useState(initialWeather);
     const fetchWeather = async () => {
-        if (!city) return;
+        if (!Thecity) return;
         try {
-            const apiKey = process.env.API_KEY ;
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+            console.log(Thecity);
+            const apiKey = process.env.NEXT_PUBLIC_API_KEY|| '' ;
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Thecity}&appid=${apiKey}&units=metric`);
             if (!response.ok) {
                 throw new Error('City not found');
             }
             const data = await response.json();
             console.log(data);
+            setWeather({
+                city: data.name,
+                temperature: data.main.temp,
+                description: data.weather[0].description,
+                condition: data.weather[0].description,
+                icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+            });
         } catch (error) {
             console.error('Error fetching weather data:', error);
         }
     }
   return (
-    <div className=" flex mt-7 backdrop-blur-md rounded-xl bg-white/20 p-6 w-full max-w-2xl ">
-        <div className="relative p-3 flex items-start w-full">
-            <MapPinIcon className="absolute mt-3 ms-2 w-5 h-5 text-gray-500 mr-2 inline" />
-            <Input
-                onChange={(e) => setCity(e.target.value)}
-                value={city}
-                type="text"
-                placeholder="Enter city name ..."
-                className=" h-12 w-full pl-9 bg-white border-none focus:border-none active:border-none focus:outline-none outline-none text-gray-800 placeholder-gray-500"></Input>
-                <div className="ms-5 mt-1 backdrop-blur-md rounded-md  p-1">
-                    <Button
-                    className="border-none outline-none cursor-pointer text-white/40 hover:text-white/80 transition-colors duration-300"
-                    onClick={fetchWeather}  
-                    ><Search></Search></Button>
-                </div>
+    <>
+        <div className=" flex mt-7 backdrop-blur-md rounded-xl bg-white/20 p-6 w-full max-w-2xl ">
+            <div className="relative p-3 flex items-start w-full">
+                <MapPinIcon className="absolute mt-3 ms-2 w-5 h-5 text-gray-500 mr-2 inline" />
+                <Input
+                    onChange={(e) => setCity(e.target.value)}
+                    value={Thecity}
+                    type="text"
+                    placeholder="Enter city name ..."
+                    className=" h-12 w-full pl-9 bg-white border-none focus:border-none active:border-none focus:outline-none outline-none text-gray-800 placeholder-gray-500"></Input>
+                    <div className="ms-5 mt-1 backdrop-blur-md rounded-md  p-1">
+                        <Button
+                        className="border-none outline-none cursor-pointer text-white/40 hover:text-white/80 transition-colors duration-300"
+                        onClick={fetchWeather}  
+                        ><Search></Search></Button>
+                    </div>
+            </div>
+        
         </div>
-        <div></div>
-    </div>
+        <div>
+            <WeatherCard weatherProp={Theweather}></WeatherCard>
+       
+        </div>
+    </>
   )}
