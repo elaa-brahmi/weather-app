08455874104c756f42cd39a173d46fd5c7c13da6
elaa <elaa.brahmii@gmail.com>
2025-07-01 17:48:48 +0100

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import WeatherCard from "@/components/home/WeatherCard";
+import InputField from "@/components/home/InputField";
 
 export default function Home() {
   return (
-   <div>home</div>
+   <div className="flex flex-col items-center min-h-screen bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 transition-all duration-1000 ease-in-out bg-black/10 backdrop-blur-sm">
+    <div className="mt-12 mb-8 text-center">
+      <h1 className="font-bold md:text-6xl text-2xl text-white ">WeatherNow</h1>
+    </div>
+    <div className="text-white font-semibold text-lg">Get real-time weather information for any city</div>
+    <InputField/>
+    <WeatherCard/>
+   </div>
   );
 }
